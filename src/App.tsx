392ec09cd1651188dd085b449c352ed7581490e4
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-11-13 20:34:57 -0300

    Cookbook 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from "./contexts/AuthContext";
+
 import Home from "./pages/home/Home"
 import Login from './pages/login/Login';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Cadastro from './pages/cadastro/Cadastro';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-    return (
+  return (
       <AuthProvider>
         <BrowserRouter>
           <Navbar />
-            <div className='min-h-[80vh]'>
-              <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/cadastro" element={<Cadastro />} />
-              </Routes>
-            </div>  
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+          </div>
           <Footer />
         </BrowserRouter>
       </ AuthProvider>
-    );
-  }
-  
-  export default App;
-  
+  );
+}
+
+export default App;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,10 +8,10 @@ function Footer() {
         <>
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <p className='text-xl font-bold'>Blog Pessoal Flávio | Copyright: {data}</p>
+
+                    <p className='text-lg'>Acesse minhas redes sociais</p>
+                    
                     <div className='flex gap-2'>
                         <LinkedinLogo size={48} weight='bold' />
                         <InstagramLogo size={48} weight='bold' />
