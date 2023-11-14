90daa6b763cbd1c8f7790e9659abe72f65e1f596
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-11-14 13:47:36 -0300

    Terminei

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { AuthProvider } from "./contexts/AuthContext";
 
 import Home from "./pages/home/Home"
 import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
-import Cadastro from './pages/cadastro/Cadastro';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 
 function App() {
   return (
@@ -23,6 +27,11 @@ function App() {
             <Route path="/temas" element={<ListaTemas />} />
             <Route path="/cadastroTema" element={<FormularioTema />} />
             <Route path="/editarTema/:id" element={<FormularioTema />} />
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            <Route path="/postagens" element={<ListaPostagens />} />
+            <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+            <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
           </Routes>
         </div>
         <Footer />
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,31 +1,37 @@
+import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem"
+
 function Home() {
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
-                <div className="container grid grid-cols-2 text-white">
+                <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
-                        <h2 className="text-5xl font-bold">
-                            Seja Bem Vinde !
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
                         </h2>
-
-                        <p className="text-xl">
+                        <p className='text-xl'>
                             Expresse aqui seus pensamentos e opniões
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <button className="rounded bg-white text-blue-800 py-2 px-4">
-                                Ver postagens
-                            </button>
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
+                            </div>
                         </div>
                     </div>
 
-                    <div className="flex justify-center">
-                        <img src="https://i.imgur.com/fyfri1v.png"
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://i.imgur.com/fyfri1v.png"
                             alt="Imagem Página Home"
-                            className="w-2/3" />
+                            className='w-2/3'
+                        />
                     </div>
                 </div>
             </div>
+
+            <ListaPostagens />
         </>
     )
 }
